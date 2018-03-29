2f730ffb64993a57685e50740a620258345afb92
Ika Javakhishvili <ikajava@gmail.com>
2018-03-30 00:11:17 +0400

    added map

--- MapKitView/detailsViewController.swift
+++ MapKitView/detailsViewController.swift
@@ -7,35 +7,71 @@
 //
 
 import UIKit
+import MapKit
 
-class detailsViewController: UIViewController {
+class detailsViewController: UIViewController, MKMapViewDelegate  {
+   
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var restaurant: Restaurant?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        searchBar.delegate = self
         
         if let restaurant = restaurant {
             print(restaurant.latitude)
+            
+            
+            let annotation = MKPointAnnotation()
+            let coordinate = CLLocationCoordinate2DMake(Double(restaurant.latitude)!, Double(restaurant.longitude)!)
+            let span = MKCoordinateSpanMake(0.06, 0.06)
+            let region = MKCoordinateRegionMake(coordinate, span)
+            
+            mapView.setRegion(region, animated: true)
+            
+            annotation.coordinate = coordinate
+            annotation.title = "Restaurant"
+            mapView.addAnnotation(annotation)
+            mapView.selectAnnotation(annotation, animated: true)
         }
-
-        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    @IBAction func closeButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension detailsViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(searchBar.text!) { (placemarks, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                guard let placemark = placemarks?.first else {
+                    return
+                }
+                
+                let annotation = MKPointAnnotation()
+                var coordinate = CLLocationCoordinate2D()
+                coordinate = (placemark.location?.coordinate)!
+                annotation.coordinate = coordinate
+                annotation.title = "\(String(describing: placemark.country!)) \n \(String(describing: placemark.name!))"
+                let span = MKCoordinateSpanMake(0.01, 0.01)
+                let region = MKCoordinateRegionMake(annotation.coordinate, span)
+                
+                self.mapView.setRegion(region, animated: true)
+                self.mapView.addAnnotation(annotation)
+                self.mapView.selectAnnotation(annotation, animated: true)
+                
+            }
+        }
     }
-    */
-
 }
